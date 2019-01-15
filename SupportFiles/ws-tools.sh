28449799ad9afb8b8de749a576a7d1a3ae83fe03
vivien.s.chen <vivien.chen@plus3it.com>
2019-01-14 19:02:43 -0500

    Added implementation for a few app installation

--- SupportFiles/ws-tools.sh
+++ SupportFiles/ws-tools.sh
@@ -39,26 +39,78 @@ setenforce 1
 # Unzip the tools.tar.gz
 tar xfz /etc/cfn/tools/tools.tar.gz -C /etc/cfn/tools || err_exit "Failed to unzip tools.tar.gz"
 
+# Install Anaconda
+bash /etc/cfn/tools/anaconda/anaconda.sh -b -p /home/maintuser/anaconda3
+chown -R maintuser:maintuser /home/maintuser/anaconda3
+
+cp /etc/cfn/tools/anaconda/anaconda.desktop /usr/share/applications/anaconda.desktop
+cp /etc/cfn/tools/anaconda/anaconda.desktop /home/maintuser/.local/share/applications/anaconda.desktop
+chown maintuser:maintuser /home/maintuser/.local/share/applications/anaconda.desktop
+chmod 600 /home/maintuser/.local/share/applications/anaconda.desktop
+
 # Install ATOM
 # Launch it from your terminal by running the command "atom"
-yum -y install /etc/cfn/tools/atom.x86_64.rpm || err_exit "Failed to install ATOM"
-
-# (May not need to do this) Put it in the user's .local/share/applications/atom.desktop
-#cp /usr/share/applications/atom.desktop /home/ProvisionUser/.local/share/applications/atom.desktop
-#chmod go-rwx /home/ProvisionUser/.local/share/applications
-#chown -R ProvisionUser:ProvisionUser /home/ProvisionUser/.local/share/applications
+yum -y install /etc/cfn/tools/atom/atom.x86_64.rpm || err_exit "Failed to install ATOM"
 
 #Install Eclipse NEON IDE for Java EE Developers
-tar xfz /etc/cfn/tools/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz -C /opt/ || err_exit "Failed to unzip eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz"
+tar xfz /etc/cfn/tools/eclipse/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz -C /opt/ || err_exit "Failed to unzip eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz"
 ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
-cp /etc/cfn/tools/eclipse.desktop /usr/share/applications/eclipse.desktop
-cp /etc/cfn/tools/eclipse.desktop /home/maintuser/.local/share/applications/eclipse.desktop
-chmod 755 /home/maintuser/.local/share/applications/eclipse.desktop
+
+cp /etc/cfn/tools/eclipse/eclipse.desktop /usr/share/applications/eclipse.desktop
+cp /etc/cfn/tools/eclipse/eclipse.desktop /home/maintuser/.local/share/applications/eclipse.desktop
+chown maintuser:maintuser /home/maintuser/.local/share/applications/eclipse.desktop
+chmod 600 /home/maintuser/.local/share/applications/eclipse.desktop
 
 #Install Intellij
-tar xfz /etc/cfn/tools/ideaIC-2018.3.3.tar.gz -C /opt/ || err_exit "Failed to unzip ideaIC-2018.3.3.tar.gz"
-ln -s /opt/idea-IC-183.5153.38/bin/idea.sh /usr/local/bin/idea.sh
+tar xfz /etc/cfn/tools/intellij/ideaIC-2018.3.3.tar.gz -C /opt/ || err_exit "Failed to unzip ideaIC-2018.3.3.tar.gz"
+chmod -R 755 /opt/idea-IC-183.5153.38
+ln -s /opt/idea-IC-183.5153.38/bin/idea.sh /usr/local/bin/idea
+
+cp /etc/cfn/tools/intellij/jetbrains-idea-ce.desktop /usr/share/applications/jetbrains-idea-ce.desktop
+cp /etc/cfn/tools/intellij/jetbrains-idea-ce.desktop /home/maintuser/.local/share/applications/jetbrains-idea-ce.desktop
+chown maintuser:maintuser /home/maintuser/.local/share/applications/jetbrains-idea-ce.desktop
+chmod 600 /home/maintuser/.local/share/applications/jetbrains-idea-ce.desktop
 
 #Install emacs
 yum -y install emacs || err_exit "Failed to install emacs"
+
+#Install Gradle
+unzip -d /opt/ /etc/cfn/tools/gradle/gradle-5.1.1-bin.zip
+chmod -R 755 /opt/gradle-5.1.1
+ln -s /opt/gradle-5.1.1/bin/gradle /usr/local/bin/gradle
+
+# Install Maven
+yum -y install maven || err_exit "Failed to install maven"
+
+# Install git
+yum -y install git  || err_exit "Failed to install git"
+yum -y install git-gui || err_exit "Failed to install git-gui"
+
+# Install ruby
+yum -y install ruby || err_exit "Failed to install ruby"
+
+# Install node.js
+tar -xvf  /etc/cfn/tools/nodejs/node-v11.6.0-linux-x64.tar.xz -C /opt
+chmod -R 755 /opt/node-v11.6.0-linux-x64
+ln -s /opt/node-v11.6.0-linux-x64/bin/node /usr/bin/node
+ln -s /opt/node-v11.6.0-linux-x64/bin/npm /usr/bin/npm
+
+# Install pycharm
+tar -xvf  /etc/cfn/tools/pycharm/pycharm-community-2018.3.3.tar.gz -C /opt
+chmod -R 755 /opt/pycharm-community-2018.3.3
+ln -s /opt/pycharm-community-2018.3.3/bin/pycharm.sh /usr/local/bin/pycharm 
+
+cp /etc/cfn/tools/pycharm/pycharm.desktop /usr/share/applications/pycharm.desktop
+cp /etc/cfn/tools/pycharm/pycharm.desktop /home/maintuser/.local/share/applications/pycharm.desktop
+chown maintuser:maintuser /home/maintuser/.local/share/applications/pycharm.desktop
+chmod 600 /home/maintuser/.local/share/applications/pycharm.desktop                                                       
+
+#cp /etc/cfn/tools/gradle/gradle.sh  /etc/profile.d/gradle.sh
+#chmod 755 /etc/profile.d/gradle.sh
+
+#ln -s /opt/gradle/gradle-5.1.1/bin/gradle /usr/local/bin/gradle
+
+# (TODO) add app to PATH
+# sed -i 's;^PATH=.*;PATH='"$PATH"';' .bashrc
+# echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
 
